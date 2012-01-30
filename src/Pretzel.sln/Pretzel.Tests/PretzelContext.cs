33f7f40e978b445c379341ac533dbf1efceed12b
Brendan Forster <me@brendanforster.com>
2012-01-30 23:13:47 +1100

    added simple test and R# config

---
+++ src/Pretzel.sln/Pretzel.Tests/PretzelContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Pretzel.Tests
+{
+    public abstract class SpecificationFor<T>
+    {
+        public T Subject;
+
+        public abstract T Given();
+        public abstract void When();
+
+        protected SpecificationFor()
+        {
+            Subject = Given();
+            When();
+        }
+    }
+}
---
+++ src/Pretzel.sln/Pretzel.Tests/SampleTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Pretzel.Tests
+{
+    public class SampleClass { }
+
+    public class SampleTest : SpecificationFor<SampleClass>
+    {
+        public override SampleClass Given()
+        {
+            return new SampleClass();
+        }
+
+        public override void When()
+        {
+            Assert.True(true);
+        }
+    }
+}
