f7d48d88e08d8f03df34934be287d7089ae7b72a
aeoth <paul@theleagueofpaul.com>
2012-01-31 12:44:48 +1100

    Initial YAML Front Matter (https://github.com/mojombo/jekyll/wiki/yaml-front-matter) processing. Returns a dictionary of the header values.

--- src/Pretzel/Program.cs
+++ src/Pretzel/Program.cs
@@ -2,13 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using YamlDotNet.RepresentationModel;
+using System.IO;
+using System.Text.RegularExpressions;
+using Pretzel.Logic.Extensions;
 
 namespace Pretzel
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
+            var text = @"---
+                        layout: post
+                        title: This is a test jekyll document
+                        description: TEST ALL THE THINGS
+                        date: 2012-01-30
+                        tags : 
+                        - test
+                        - alsotest
+                        - lasttest
+                        ---
+
+                        ##Test
+
+                        This is a test of YAML parsing";
+
+            var header = text.YamlHeader();
+
         }
+
+
     }
 }
