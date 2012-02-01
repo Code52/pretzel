c193d7dad1a84eb17599c1d8062f48327f4bc9a1
Konrad Kruczyński <konrad.kruczynski@gmail.com>
2012-02-01 13:02:06 +0100

    NuGet resolved in build script for Mono.

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 config=${1-Debug}
+
+function nugetit {
+  olddir=`pwd`
+  cd $1
+  mono --runtime=v4.0 ../.nuget/NuGet.exe install packages.config
+  cd $olddir
+}
+
+cd src
+nugetit ./Pretzel
+nugetit ./Pretzel.Logic
+nugetit ./Pretzel.Tests
+cd ..
 xbuild build.proj /t:Test /p:Configuration=$config /v:M /flp:LogFile=msbuild.log;Verbosity=Normal
+
