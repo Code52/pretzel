a76d48598d84cd9024f68d6fe7b825cdf95fd850
Brendan Forster <me@brendanforster.com>
2012-02-01 17:22:28 +1100

    and there's a couple of basic tests to start us off

--- src/Pretzel.Logic/Templating/Liquid/LiquidEngine.cs
+++ src/Pretzel.Logic/Templating/Liquid/LiquidEngine.cs
@@ -1,11 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
+using System.IO.Abstractions;
 
 namespace Pretzel.Logic.Templating.Liquid
 {
     public class LiquidEngine : ITemplateEngine
     {
+        public void Process(IFileSystem fileSystem, string folder)
+        {
+            var outputPath = Path.Combine(folder, "_site");
+            fileSystem.Directory.CreateDirectory(outputPath);
+
+            foreach (var file in fileSystem.Directory.GetFiles(folder))
+            {
+                var fileName = Path.GetFileName(file);
+                var newPath = Path.Combine(outputPath, fileName);
+                fileSystem.File.WriteAllText(newPath, fileSystem.File.ReadAllText(file));
+            }
+        }
     }
 }
--- src/Pretzel.Tests/Templating/Liquid/LiquidEngineTests.cs
+++ src/Pretzel.Tests/Templating/Liquid/LiquidEngineTests.cs
@@ -1,17 +1,52 @@
-﻿using Pretzel.Logic.Templating.Liquid;
+﻿using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using Pretzel.Logic.Templating.Liquid;
+using Xunit;
 
 namespace Pretzel.Tests.Templating.Liquid
 {
-    public class LiquidEngineTests :SpecificationFor<LiquidEngine>
+    public class LiquidEngineTests
     {
-        public override LiquidEngine Given()
+        public class When_Recieving_A_Folder_Containing_One_File : SpecificationFor<LiquidEngine>
         {
-            return new LiquidEngine();
-        }
+            MockFileSystem fileSystem;
+            const string fileContents = "<html><head></head><body></body></html>";
 
-        public override void When()
-        {
-            // no - op
+            public override LiquidEngine Given()
+            {
+                return new LiquidEngine();
+            }
+
+            public override void When()
+            {
+                var filepath = @"C:\website\index.html";
+                
+
+                fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+                                                {
+                                                    {filepath, new MockFileData(fileContents)}
+                                                });
+
+                Subject.Process(fileSystem, @"C:\website\");
+            }
+
+            [Fact]
+            public void That_Site_Folder_Is_Created()
+            {
+                Assert.True(fileSystem.Directory.Exists(@"C:\website\_site"));
+            }
+
+            [Fact]
+            public void The_File_Is_Added_At_The_Root()
+            {
+                Assert.True(fileSystem.File.Exists(@"C:\website\_site\index.html"));
+            }
+
+            [Fact]
+            public void The_File_Is_Identical()
+            {
+                Assert.Equal(fileContents, fileSystem.File.ReadAllText(@"C:\website\_site\index.html"));
+            }
         }
     }
 }
