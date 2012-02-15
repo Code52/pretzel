0e44e3666770412abf2bbb2b5c3d22888d3c7a85
Mark Heath <mark.heath@gmail.com>
2012-02-15 15:05:48 +0000

    more bugfixes to blogger import

--- src/Pretzel.Logic/Import/HtmlToMarkdownConverter.cs
+++ src/Pretzel.Logic/Import/HtmlToMarkdownConverter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using HtmlAgilityPack;
+using System.Text.RegularExpressions;
 
 namespace Pretzel.Logic.Import
 {
@@ -20,6 +21,13 @@ namespace Pretzel.Logic.Import
             return markdown.ToString();
         }
 
+        private static Regex regexBr = new Regex(@"\<br\s*/?\>",
+            RegexOptions.IgnoreCase
+            | RegexOptions.CultureInvariant
+            | RegexOptions.IgnorePatternWhitespace
+            | RegexOptions.Compiled);
+
+
         private static void ProcessNodes(StringBuilder markdown, IEnumerable<HtmlNode> htmlNodes)
         {
             foreach (var htmlNode in htmlNodes)
@@ -36,6 +44,20 @@ namespace Pretzel.Logic.Import
                     case "h2":
                         markdown.AppendFormat("## {0}", htmlNode.InnerText);
                         markdown.AppendLine();
+                        break;
+                    case "h3":
+                        markdown.AppendFormat("### {0}", htmlNode.InnerText);
+                        markdown.AppendLine();
+                        break;
+                    case "ul":
+                        markdown.AppendLine();
+                        ProcessNodes(markdown, htmlNode.ChildNodes);
+                        markdown.AppendLine();
+                        break;
+                    case "li":
+                        markdown.AppendLine();
+                        markdown.Append("* ");
+                        ProcessNodes(markdown, htmlNode.ChildNodes);
                         break;
                     case "p":
                         markdown.AppendLine();
@@ -62,7 +84,12 @@ namespace Pretzel.Logic.Import
                         break;
                     case "pre":
                     case "code":
-                        var lines = System.Text.RegularExpressions.Regex.Split(htmlNode.InnerText, Environment.NewLine);
+                        var code = htmlNode.InnerText;
+                        // a bit hacky, but we need to sort out where lines of code end
+                        code = code.Replace("\r\n", "\n");
+                        code = code.Replace("\r", "\n");
+                        code = regexBr.Replace(code, "\n");
+                        var lines = code.Split('\n');
                         markdown.Append(Environment.NewLine + "    ");
                         markdown.Append(string.Join(Environment.NewLine + "    ", lines));
                         break;
