02e15e3c618d2aec02a2179c0442c3e7eee2ea98
Mark Heath <mark.heath@gmail.com>
2012-02-15 11:28:59 +0000

    added an XLST based XHTML to Markdown converter

---
+++ src/Pretzel.Logic/Import/XhtmlToMarkdownConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+
+namespace Pretzel.Logic.Import
+{
+    public class XhtmlToMarkdownConverter
+    {
+        public static string Convert(string html)
+        {
+            // going to use an XSLT by Andrew Green (used with permission)
+            // http://www.lowerelement.com/Geekery/XML/XHTML-to-Markdown.html
+            
+            using (StringReader sReader = new StringReader(html))
+            using (XmlReader xReader = XmlReader.Create(sReader))
+            using (StringWriter sWriter = new StringWriter())
+            {
+                XslCompiledTransform xslTransform = new XslCompiledTransform();
+                var xslStream = GetEmbeddedResourceStream("Pretzel.Logic.Import.markdown.xsl");
+                xslTransform.Load(XmlReader.Create(xslStream));
+                xslTransform.Transform(xReader, null, sWriter);
+                return sWriter.ToString();
+            }
+            // n.b. could also try this, if XSLT approach is not suitable
+            // http://htmlagilitypack.codeplex.com/
+
+        }
+
+        public static Stream GetEmbeddedResourceStream(string resourceName)
+        {
+            return Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+        }
+
+    }
+}
---
+++ src/Pretzel.Tests/Import/XhtmlToMarkdownConverterTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Pretzel.Logic.Import;
+
+namespace Pretzel.Tests.Import
+{
+    public class XhtmlToMarkdownConverterTests
+    {
+        [Fact]
+        public void P_elements_are_converted()
+        {
+            string md = XhtmlToMarkdownConverter.Convert("<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\"><head><title>blah</title></head><body><h1>Hello world</h1><p>Something</p>Paragraph 2</body></html>");
+            Assert.Equal("Hello world", md);
+        }
+    }
+}
