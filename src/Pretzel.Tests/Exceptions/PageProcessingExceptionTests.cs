e3e91a85267a4a4dc160c53e83556b3e8fbc96ac
Brendan Forster <me@brendanforster.com>
2012-03-17 16:03:58 +1100

    tests for custom exception

---
+++ src/Pretzel.Tests/Exceptions/PageProcessingExceptionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Pretzel.Logic.Exceptions;
+using Xunit;
+
+namespace Pretzel.Tests.Exceptions
+{
+    public class PageProcessingExceptionTests
+    {
+        [Fact]
+        public void Constructor_WithMessage_IsCorrectStructure()
+        {
+            var expected = "Some Message";
+            var ex = new PageProcessingException(expected);
+            Assert.Equal(expected, ex.Message);
+        }
+
+        [Fact]
+        public void Constructor_WithInnerAndMessage_IsCorrectStructure()
+        {
+            var expected = "Some Message";
+            var inner = new Exception();
+            var ex = new PageProcessingException(expected, inner);
+            Assert.Equal(expected, ex.Message);
+            Assert.Same(inner, ex.InnerException);
+        }
+    }
+}
