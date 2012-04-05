bee0f25d7adbeba870eed489f8fbd766b58ce49b
Andrei Marukovich <andrei.marukovich@lunarfrog.com>
2012-04-05 15:28:58 -0400

    Add _inludes support for Razor

---
+++ src/Pretzel.Logic/Templating/Razor/IncludesResolver.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.IO.Abstractions;
+using RazorEngine.Templating;
+
+namespace Pretzel.Logic.Templating.Razor
+{
+	internal class IncludesResolver : ITemplateResolver
+	{
+		private readonly IFileSystem fileSystem;
+		private readonly string includesPath;
+
+		public IncludesResolver(IFileSystem fileSystem, string includesPath)
+		{
+			this.fileSystem = fileSystem;
+			this.includesPath = includesPath;
+		}
+
+		public string Resolve(string name)
+		{
+			var templatePath = Path.Combine(includesPath, name);
+			var templateExists = fileSystem.File.Exists(templatePath);
+			if (!templateExists)
+			{
+				foreach (var ext in new[] {".cshtml", ".html", ".html"})
+				{
+					var testPath = String.Concat(templatePath, ext);
+					templateExists = fileSystem.File.Exists(testPath);
+					if (templateExists)
+					{
+						templatePath = testPath;
+						break;
+					}
+				}
+			}
+
+			return templateExists ? fileSystem.File.ReadAllText(templatePath) : String.Empty;
+		}
+	}
+}
