942e0750fb535b158f9f99a92abb8c49f7badfb4
Andrei Marukovich <andrei.marukovich@lunarfrog.com>
2012-04-07 22:38:57 -0400

    Parse header tags and categories, populate site context

--- src/Pretzel.Tests/Templating/Context/SiteContextGeneratorTests.cs
+++ src/Pretzel.Tests/Templating/Context/SiteContextGeneratorTests.cs
@@ -136,5 +136,71 @@ namespace Pretzel.Tests.Templating.Context
 title: Title
 ---" + content;
         }
+
+        [Fact]
+        public void Generator_Extracts_Tags_From_Posts()
+        {
+            fileSystem.AddFile(@"C:\TestSite\_posts\2012-01-01-File1.md", new MockFileData("---\n\r tags: [\"tag1\",\"tag2\"]\n\r---\n\r Test"));
+            fileSystem.AddFile(@"C:\TestSite\_posts\2012-01-02-File2.md", new MockFileData("---\n\r tags: [\"tag2\",\"tag3\"]\n\r---\n\r Test"));
+            fileSystem.AddFile(@"C:\TestSite\_posts\2012-01-03-File3.md", new MockFileData("---\n\r tags: [\"tag4\"]\n\r categories: [\"cat1\"]\n\r---\n\r Test"));
+            fileSystem.AddFile(@"C:\TestSite\page.md", new MockFileData("---\n\r tags: [\"tag2\",\"tag3\"]\n\r categories: [\"cat1\"]\n\r---\n\r Test"));
+
+            var siteContext = generator.BuildContext(@"C:\TestSite");
+
+            Assert.Equal(4, siteContext.Tags.Count());
+            Assert.Equal(1, siteContext.Categories.Count());
+            
+            var tag1 = siteContext.Tags.First(x => x.Name == "tag1");
+            Assert.Equal(1, tag1.Posts.Count());
+            Assert.Equal(2, tag1.Posts.First().Tags.Count());
+            Assert.True(tag1.Posts.First().File.Contains("File1"));
+
+            var tag2 = siteContext.Tags.First(x => x.Name == "tag2");
+            Assert.Equal(2, tag2.Posts.Count());
+            Assert.NotNull(tag2.Posts.FirstOrDefault(x=>x.File.Contains("File1")));
+            Assert.NotNull(tag2.Posts.FirstOrDefault(x=>x.File.Contains("File2")));
+            
+            var tag3 = siteContext.Tags.First(x => x.Name == "tag3");
+            Assert.Equal(1, tag3.Posts.Count());
+            Assert.True(tag3.Posts.First().File.Contains("File2"));
+            
+            var tag4 = siteContext.Tags.First(x => x.Name == "tag4");
+            Assert.Equal(1, tag4.Posts.Count());
+            Assert.Equal(1, tag4.Posts.First().Tags.Count());
+            Assert.True(tag4.Posts.First().File.Contains("File3"));
+        }
+
+        [Fact]
+        public void Generator_Extracts_Categories_From_Posts()
+        {
+            fileSystem.AddFile(@"C:\TestSite\_posts\2012-01-01-File1.md", new MockFileData("---\n\r categories: [\"cat1\",\"cat2\"]\n\r---\n\r Test"));
+            fileSystem.AddFile(@"C:\TestSite\_posts\2012-01-02-File2.md", new MockFileData("---\n\r categories: [\"cat2\",\"cat3\"]\n\r---\n\r Test"));
+            fileSystem.AddFile(@"C:\TestSite\_posts\2012-01-03-File3.md", new MockFileData("---\n\r categories: [\"cat4\"]\n\r tags: [\"tag1\"]\n\r---\n\r Test"));
+            fileSystem.AddFile(@"C:\TestSite\page.md", new MockFileData("---\n\r categories: [\"cat2\",\"cat3\"]\n\r tags: [\"tag1\"]\n\r---\n\r Test"));
+
+            var siteContext = generator.BuildContext(@"C:\TestSite");
+
+            Assert.Equal(4, siteContext.Categories.Count());
+            Assert.Equal(1, siteContext.Tags.Count());
+            
+            var cat1 = siteContext.Categories.First(x => x.Name == "cat1");
+            Assert.Equal(1, cat1.Posts.Count());
+            Assert.Equal(2, cat1.Posts.First().Categories.Count());
+            Assert.True(cat1.Posts.First().File.Contains("File1"));
+
+            var cat2 = siteContext.Categories.First(x => x.Name == "cat2");
+            Assert.Equal(2, cat2.Posts.Count());
+            Assert.NotNull(cat2.Posts.FirstOrDefault(x=>x.File.Contains("File1")));
+            Assert.NotNull(cat2.Posts.FirstOrDefault(x=>x.File.Contains("File2")));
+
+            var cat3 = siteContext.Categories.First(x => x.Name == "cat3");
+            Assert.Equal(1, cat3.Posts.Count());
+            Assert.True(cat3.Posts.First().File.Contains("File2"));
+
+            var cat4 = siteContext.Categories.First(x => x.Name == "cat4");
+            Assert.Equal(1, cat4.Posts.Count());
+            Assert.Equal(1, cat4.Posts.First().Categories.Count());
+            Assert.True(cat4.Posts.First().File.Contains("File3"));
+        }
     }
 }
