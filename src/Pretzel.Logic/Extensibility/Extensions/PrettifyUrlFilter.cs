1e8b4e336a49bbb8a66a781fc3bf61119d51b37a
Andrei Marukovich <andrei.marukovich@lunarfrog.com>
2012-05-01 15:22:30 -0400

    Added PrettifyUrlFilter

---
+++ src/Pretzel.Logic/Extensibility/Extensions/PrettifyUrlFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+namespace Pretzel.Logic.Extensibility.Extensions
+{
+	[Export(typeof(IFilter))]
+	public class PrettifyUrlFilter : IFilter
+	{
+		public string Name
+		{
+			get { return "PrettifyUrl"; }
+		}
+
+		public static string PrettifyUrl(string input)
+		{
+			return input.Replace("index.html", String.Empty);
+		}
+
+	}
+}
