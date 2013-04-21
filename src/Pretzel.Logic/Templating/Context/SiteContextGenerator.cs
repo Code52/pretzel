5eee8761df7408d7a9d0c8d9d9e9947bd23098a2
Brendan Forster <me@brendanforster.com>
2013-04-21 21:52:57 +1000

    of course SublimeText does something different with CSS files. of course

--- src/Pretzel.Logic/Templating/Context/SiteContextGenerator.cs
+++ src/Pretzel.Logic/Templating/Context/SiteContextGenerator.cs
@@ -157,7 +157,7 @@ namespace Pretzel.Logic.Templating.Context
 
         private static bool IsSpecialPath(string relativePath)
         {
-            return relativePath.StartsWith("_") || (relativePath.StartsWith(".") && relativePath != ".htaccess") || relativePath.EndsWith(".TMP");
+            return relativePath.StartsWith("_") || (relativePath.StartsWith(".") && relativePath != ".htaccess") || relativePath.EndsWith(".TMP", StringComparison.OrdinalIgnoreCase);
         }
 
         private Page CreatePage(SiteContext context, IDictionary<string, object> config, string file, bool isPost)
