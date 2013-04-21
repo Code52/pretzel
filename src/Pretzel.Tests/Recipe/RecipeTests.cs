60ae12391e511db68768e9d2ff6bbe818d9b3241
Jake Ginnivan <jake.ginnivan@readify.net>
2013-04-21 14:54:52 +0800

    Added Wiki switch to create a wiki instead of a blog

--- src/Pretzel.Tests/Recipe/RecipeTests.cs
+++ src/Pretzel.Tests/Recipe/RecipeTests.cs
@@ -8,6 +8,7 @@ using NSubstitute;
 using Pretzel.Logic.Extensibility;
 using Pretzel.Logic.Extensions;
 using Xunit;
+using Xunit.Extensions;
 
 namespace Pretzel.Tests.Recipe
 {
@@ -29,7 +30,7 @@ namespace Pretzel.Tests.Recipe
         [Fact]
         public void Files_and_Folders_Are_Created_for_Jekyll()
         {
-            var recipe = new Logic.Recipe.Recipe(fileSystem, "liquid", BaseSite, Enumerable.Empty<IAdditionalIngredient>(), false);
+            var recipe = new Logic.Recipe.Recipe(fileSystem, "liquid", BaseSite, Enumerable.Empty<IAdditionalIngredient>(), false, false);
             recipe.Create();
 
             Assert.True(fileSystem.Directory.Exists(BaseSite + @"_posts\"));
@@ -53,29 +54,31 @@ namespace Pretzel.Tests.Recipe
             Assert.True(writer.ToString().Contains("Pretzel site template has been created"));
         }
 
-        [Fact]
-        public void Files_and_Folders_Are_Created_for_Razor()
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void Files_and_Folders_Are_Created_for_Razor(bool wiki)
         {
-            var recipe = new Logic.Recipe.Recipe(fileSystem, "razor", BaseSite, Enumerable.Empty<IAdditionalIngredient>(), false);
+            var recipe = new Logic.Recipe.Recipe(fileSystem, "razor", BaseSite, Enumerable.Empty<IAdditionalIngredient>(), false, wiki);
             recipe.Create();
 
             Assert.True(fileSystem.Directory.Exists(BaseSite + @"_posts\"));
             Assert.True(fileSystem.Directory.Exists(BaseSite + @"_layouts\"));
             Assert.True(fileSystem.Directory.Exists(BaseSite + @"css\"));
             Assert.True(fileSystem.Directory.Exists(BaseSite + @"img\"));
-
-            Assert.True(fileSystem.File.Exists(BaseSite + "rss.xml"));
-            Assert.True(fileSystem.File.Exists(BaseSite + "atom.xml"));
             Assert.True(fileSystem.File.Exists(BaseSite + @"_layouts\layout.cshtml"));
-            Assert.True(fileSystem.File.Exists(BaseSite + @"_layouts\post.cshtml"));
             Assert.True(fileSystem.File.Exists(BaseSite + "index.md"));
-            Assert.True(fileSystem.File.Exists(BaseSite + "about.md"));
-            Assert.True(fileSystem.File.Exists(BaseSite + string.Format(@"_posts\{0}-myfirstpost.md", DateTime.Today.ToString("yyyy-MM-dd"))));
             Assert.True(fileSystem.File.Exists(BaseSite + @"css\style.css"));
-            Assert.True(fileSystem.File.Exists(BaseSite + @"img\25.png"));
-            Assert.True(fileSystem.File.Exists(BaseSite + @"img\favicon.png"));
-            Assert.True(fileSystem.File.Exists(BaseSite + @"img\logo.png"));
             Assert.True(fileSystem.File.Exists(BaseSite + @"img\favicon.ico"));
+
+            Assert.Equal(!wiki, fileSystem.File.Exists(BaseSite + @"_layouts\post.cshtml"));
+            Assert.Equal(!wiki, fileSystem.File.Exists(BaseSite + "about.md"));
+            Assert.Equal(!wiki, fileSystem.File.Exists(BaseSite + string.Format(@"_posts\{0}-myfirstpost.md", DateTime.Today.ToString("yyyy-MM-dd"))));
+            Assert.Equal(!wiki, fileSystem.File.Exists(BaseSite + @"img\25.png"));
+            Assert.Equal(!wiki, fileSystem.File.Exists(BaseSite + @"img\favicon.png"));
+            Assert.Equal(!wiki, fileSystem.File.Exists(BaseSite + @"img\logo.png"));
+            Assert.Equal(!wiki, fileSystem.File.Exists(BaseSite + "rss.xml"));
+            Assert.Equal(!wiki, fileSystem.File.Exists(BaseSite + "atom.xml"));
 
             Assert.True(writer.ToString().Contains("Pretzel site template has been created"));
         }
@@ -84,7 +87,7 @@ namespace Pretzel.Tests.Recipe
         public void Other_Engine_returns_error()
         {
             fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>());
-            var recipe = new Logic.Recipe.Recipe(fileSystem, "Musak", BaseSite, Enumerable.Empty<IAdditionalIngredient>(), false);
+            var recipe = new Logic.Recipe.Recipe(fileSystem, "Musak", BaseSite, Enumerable.Empty<IAdditionalIngredient>(), false, false);
 
             recipe.Create();
 
@@ -96,7 +99,7 @@ namespace Pretzel.Tests.Recipe
         {
             fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>());
             var additionalIngredient = Substitute.For<IAdditionalIngredient>();
-            var recipe = new Logic.Recipe.Recipe(fileSystem, "Razor", BaseSite, new[] { additionalIngredient }, false);
+            var recipe = new Logic.Recipe.Recipe(fileSystem, "Razor", BaseSite, new[] { additionalIngredient }, false, false);
             recipe.Create();
 
             additionalIngredient.Received().MixIn(BaseSite);
@@ -107,7 +110,7 @@ namespace Pretzel.Tests.Recipe
         {
             fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>());
             var additionalIngredient = Substitute.For<IAdditionalIngredient>();
-            var recipe = new Logic.Recipe.Recipe(fileSystem, "Liquid", BaseSite, new[] { additionalIngredient }, false);
+            var recipe = new Logic.Recipe.Recipe(fileSystem, "Liquid", BaseSite, new[] { additionalIngredient }, false, false);
             recipe.Create();
 
             additionalIngredient.Received().MixIn(BaseSite);
@@ -118,7 +121,7 @@ namespace Pretzel.Tests.Recipe
         {
             fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>());
             var additionalIngredient = Substitute.For<IAdditionalIngredient>();
-            var recipe = new Logic.Recipe.Recipe(fileSystem, "Musak", BaseSite, new[] { additionalIngredient }, false);
+            var recipe = new Logic.Recipe.Recipe(fileSystem, "Musak", BaseSite, new[] { additionalIngredient }, false, false);
             recipe.Create();
 
             additionalIngredient.DidNotReceive().MixIn(BaseSite);
