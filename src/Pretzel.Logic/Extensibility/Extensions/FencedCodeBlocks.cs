5c4282959d58234d49d7af51d7c207843738440e
Lodewijk Sioen <lodewijksioen@gmail.com>
2013-11-01 14:30:01 +0100

    Implemented Colorizing with fenced code blocks
    This colorizes during the Markdown parsing. Might fix #161

---
+++ src/Pretzel.Logic/Extensibility/Extensions/FencedCodeBlocks.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using Pygments;
+
+namespace Pretzel.Logic.Extensibility.Extensions
+{
+    public class FencedCodeBlocks : IContentTransform
+    {
+        private static readonly Regex FencedCodeBlokRegex = new Regex(@"(?:\n|\r|\r\n|\A)(`{3,})(?<lexer>[a-zA-Z0-9_-]+)\s*(\n|\r|\r\n)(?<code>(?>(?!\1\s*(\n|\r|\r\n)).*(\n|\r|\r\n)+)+)\1\s*(\n|\r|\r\n)*", 
+            RegexOptions.IgnorePatternWhitespace);
+        private static readonly Highlighter Highlighter = new Highlighter();
+
+        public string Transform(string content)
+        {
+            return FencedCodeBlokRegex.Replace(content, match =>
+            {
+                var lexerName = match.Groups["lexer"].Value;
+                var code = match.Groups["code"].Value;
+
+                return Highlighter.HighlightToHtml(code, lexerName, "vs", fragment: true);
+            });
+        }
+    }
+}
