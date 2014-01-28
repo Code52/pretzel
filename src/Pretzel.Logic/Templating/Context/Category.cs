9af1a6318551dc554f62898501f9612e49fa6f0d
Jordan Wallwork <jrwallwork@gmail.com>
2014-01-28 23:11:45 +0000

    Make category inherit Drop (so it can be used in templates)

--- src/Pretzel.Logic/Templating/Context/Category.cs
+++ src/Pretzel.Logic/Templating/Context/Category.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
+using DotLiquid;
 
 namespace Pretzel.Logic.Templating.Context
 {
-    public class Category
+    public class Category : Drop
     {
         public IEnumerable<Page> Posts { get; set; }
         public string Name { get; set; }
