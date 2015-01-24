dbcb3181692146b070a99acea791e7e8f26679a1
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2015-01-24 12:01:26 +0100

    The style was not used

--- src/Pretzel.Logic/Extensibility/Extensions/WebSequenceDiagrams.cs
+++ src/Pretzel.Logic/Extensibility/Extensions/WebSequenceDiagrams.cs
@@ -5,6 +5,9 @@ namespace Pretzel.Logic.Extensibility.Extensions
     public class WebSequenceDiagrams : IContentTransform
     {
         static readonly Regex SequenceDiagramRegex = new Regex(@"(?s:<pre><code>@@sequence(?<style>.*?)\r?\n(?<sequenceContent>.*?)</code></pre>)");
+        const string Style_Template = " wsd_style=\"{0}\"";
+        const string Div_Template = "<div class=\"wsd\"{1}><pre>{0}</pre></div>";
+        const string JS_Script = "\r\n<script type=\"text/javascript\" src=\"http://www.websequencediagrams.com/service.js\"></script>";
 
         public string Transform(string content)
         {
@@ -14,16 +17,19 @@ namespace Pretzel.Logic.Extensibility.Extensions
                 contentIncludesASequenceDiagram = true;
                 var sequenceContent = match.Groups["sequenceContent"].Value;
                 var styleGroup = match.Groups["style"];
-                string style;
-                if (styleGroup.Success && string.IsNullOrWhiteSpace(styleGroup.Value))
-                    style = string.Format(" wsd_style=\"{0}\"", styleGroup.Value);
-                else
-                    style = string.Empty;
-                return string.Format("<div class=\"wsd\"{1}><pre>{0}</pre></div>", sequenceContent, style);
+                string style = "default";
+                if (styleGroup.Success && !string.IsNullOrWhiteSpace(styleGroup.Value))
+                {
+                    style = styleGroup.Value.Trim();
+                }
+
+                return string.Format(Div_Template, sequenceContent, string.Format(Style_Template, style));
             });
 
             if (contentIncludesASequenceDiagram)
-                content += "\r\n<script type=\"text/javascript\" src=\"http://www.websequencediagrams.com/service.js\"></script>";
+            { 
+                content += JS_Script;
+            }
 
             return content;
         }
