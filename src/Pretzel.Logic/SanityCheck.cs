bc49131d740da214e980cfd205c52298bc11608c
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2015-01-24 11:35:24 +0100

    Add Tests for SanityCheck
    Don't know if it's worth it.
    But at least it's tested.
    
    And completing tests for JekyllEngineBase.

--- src/Pretzel.Logic/SanityCheck.cs
+++ src/Pretzel.Logic/SanityCheck.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices;
-using System.Diagnostics;
 using System.Text;
 using System.Threading;
 
