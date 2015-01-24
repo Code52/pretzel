e83a13bab409e5e5c7933a4fb42af5dc9c6491a9
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2015-01-24 09:14:37 +0100

    Fix #191 by compiling in Release configuration by default

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo Off
 set config=%1
 if "%config%" == "" (
-   set config=Debug
+   set config=Release
 )
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
