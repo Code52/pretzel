24d365572241e8840b53510735c222e1ab9d7d58
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2015-01-25 16:14:41 +0100

    Fix zip name and link for deployment

--- tools/chocolatey/chocolateyInstall.ps1
+++ tools/chocolatey/chocolateyInstall.ps1
@@ -34,6 +34,6 @@ try {
 }
 
 $packageName = 'Pretzel'
-$url = 'https://github.com/Code52/Pretzel/releases/download/{{version}}/Pretzel.{{version}}.zip'
+$url = 'https://github.com/Code52/Pretzel/releases/download/{{tag}}/Pretzel.{{version}}.zip'
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
