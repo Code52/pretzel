d46cb3b78191d6f2129a8e5f7067a2dc64e2bc44
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2015-03-30 22:35:56 +0200

    Quick'n dirty test of not depending directly of ScriptCs

---
+++ src/Pretzel.ScriptCs/ScriptCsPluginsEngine.cs
@@ -0,0 +1,14 @@
+﻿using Pretzel.ScriptCs.Contracts;
+using ScriptCs.ComponentModel.Composition;
+using System.ComponentModel.Composition.Primitives;
+
+namespace Pretzel.ScriptCs
+{
+    public sealed class ScriptCsPluginsEngine
+    {
+        public static ComposablePartCatalog GetScriptCsCatalog(ScriptCsPluginsEngineOptions options)
+        {
+            return new ScriptCsCatalog(options.PluginsFolderPath, new ScriptCsCatalogOptions { References = options.References });
+        }
+    }
+}
