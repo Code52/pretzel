ef466f23daa612b681deeb0b2f5808de76f838bf
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2015-04-17 07:25:11 +0200

    Fix Pretzel installation folder

--- tools/chocolatey/Pretzel.ScriptCs/chocolateyUninstall.ps1
+++ tools/chocolatey/Pretzel.ScriptCs/chocolateyUninstall.ps1
@@ -1,8 +1,10 @@
 $packageName = 'Pretzel.ScriptCs'
 
+Write-Debug "Uninstall $packageName"
+
 try {
   $binRoot = Get-BinRoot
-  $pretzelPath = "$binRoot\pretzel"
+  $pretzelPath = "$binRoot\Pretzel"
    
   # Remove folder
   If (Test-Path $pretzelPath){
