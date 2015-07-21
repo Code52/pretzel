df0fa7ca1100352a431fb125a78f5ca9f1eb49ab
Thiago Abreu <thiagoabreu7@gmail.com>
2015-07-21 10:46:28 -0300

    Reverting GetTitle to Regex. Fixing Regex for Unix.

--- src/Pretzel.Logic/Templating/Context/LinkHelper.cs
+++ src/Pretzel.Logic/Templating/Context/LinkHelper.cs
@@ -11,7 +11,8 @@ namespace Pretzel.Logic.Templating.Context
     [Export]
     public sealed class LinkHelper
     {
-        private static readonly Regex TimestampAndTitleFromPathRegex = new Regex(@"\\(?:(?<timestamp>\d+-\d+-\d+)-)?(?<title>[^\\]*)\.[^\.]+$", RegexOptions.Compiled);
+
+        private static readonly Regex TimestampAndTitleFromPathRegex = new Regex(@"[\\/](?:(?<timestamp>\d+-\d+-\d+)-)?(?<title>[^\\/]*)\.[^\.]+$", RegexOptions.Compiled);
         private static readonly Regex CategoryRegex = new Regex(@":category(\d*)", RegexOptions.Compiled);
         private static readonly Regex SlashesRegex = new Regex(@"/{1,}", RegexOptions.Compiled);
 
@@ -102,8 +103,7 @@ namespace Pretzel.Logic.Templating.Context
 
         public string GetTitle(string file)
         {
-            var tokens = Path.GetFileNameWithoutExtension(file).Split('-');
-            return String.Join("-", tokens.Skip(3));
+            return TimestampAndTitleFromPathRegex.Match(file).Groups["title"].Value;
         }
 
         private string GetPageTitle(string file)
