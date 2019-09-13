35dbd704d1d0ea48d1f1d02e1e6eaccd8bd59abb
Manuel Grundner <m.grundner@delegate.at>
2019-09-13 14:46:04 +0200

    fix: disable analyzer for abstract spec

--- src/Pretzel.Tests/PretzelContext.cs
+++ src/Pretzel.Tests/PretzelContext.cs
@@ -5,7 +5,9 @@ namespace Pretzel.Tests
         public T Subject;
 
         public abstract T Given();
+#pragma warning disable xUnit1013 //This is part of design, analyzer does not check for abstract methods correctly
         public abstract void When();
+#pragma warning restore xUnit1013
 
         protected SpecificationFor()
         {
