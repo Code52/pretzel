0e4e2c0dc0dbc2bb917cd745f3eb8dbdc44b7e5e
Manuel Grundner <m.grundner@delegate.at>
2019-09-30 14:56:13 +0200

    docs: added command

--- src/Pretzel.Logic/Commands/ICommand.cs
+++ src/Pretzel.Logic/Commands/ICommand.cs
@@ -2,11 +2,23 @@ using System.Threading.Tasks;
 
 namespace Pretzel.Logic.Commands
 {
+    /// <summary>
+    /// Defines a custom command for pretzel. Implementors should be decorated with the <see cref="CommandInfoAttribute"/>
+    /// Also see <see cref="Command{T}"/> for easier implementation.
+    /// </summary>
     public interface ICommand
     {
+        /// <summary>
+        /// Executes the specified command with arguments provided defined in <see cref="CommandInfoAttribute.CommandArgumentsType"/>.
+        /// </summary>
+        /// <param name="arguments">The arguments.</param>
+        /// <returns></returns>
         Task<int> Execute(ICommandArguments arguments);
     }
 
+    /// <summary>
+    /// Defines a custom command for pretzel. Implementors should be decorated with the <see cref="CommandInfoAttribute"/>
+    /// </summary>
     public abstract class Command<T> : ICommand where T : ICommandArguments
     {
         public Task<int> Execute(ICommandArguments arguments)
